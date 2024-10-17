bf8ab613ea9dae2f65443348cc11e43bd562b371
Charles <charles.binard@gmail.com>
2024-10-17 23:34:08 +0200

    Update

--- autopilot-api/app.js
+++ autopilot-api/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const cors = require("cors"); // Ajoute CORS ici
-const WebSocket = require("ws"); // WebSocket pour la communication en temps réel
-const axios = require("axios"); // Pour faire des appels HTTP externes
+const cors = require("cors");
+const WebSocket = require("ws");
+const axios = require("axios");
 const app = express();
 
-app.use(cors()); // Active CORS pour toutes les routes
-app.use(express.json()); // Supporte le parsing du JSON
+app.use(cors());
+app.use(express.json());
 
 let autopilotData = {
   mode: undefined,
@@ -17,12 +17,12 @@ let autopilotData = {
 };
 
 // Créer le serveur WebSocket
-const wss = new WebSocket.Server({ port: 8081 }); // WebSocket sur un autre port
+const wss = new WebSocket.Server({ port: 8081 });
 
 // Fonction pour envoyer les données à tous les clients connectés
 function sendToClient(client, data) {
   if (client.readyState === WebSocket.OPEN) {
-    client.send(JSON.stringify(data)); // Envoyer les données en JSON au client React
+    client.send(JSON.stringify(data));
   }
 }
 
